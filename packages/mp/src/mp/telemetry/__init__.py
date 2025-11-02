923bb04c18a604fe2d67b586f4af4a13f33cfecf
Amit Joseph <amitjoseph@google.com>
2025-11-02 10:30:50 +0200

    returned telemtry back to original

--- packages/mp/src/mp/telemetry/__init__.py
+++ packages/mp/src/mp/telemetry/__init__.py
@@ -25,8 +25,6 @@ import uuid
 from datetime import UTC, datetime
 from enum import Enum
 from typing import TYPE_CHECKING, Any, TypeAlias
-import os
-import hashlib
 
 import requests
 import typer
@@ -78,8 +76,7 @@ def track_command(mp_command_function: Callable) -> Callable:
             exit_code = e.exit_code
         except Exception as e:  # noqa: BLE001
             unexpected_exit = True
-            raw_stack = traceback.format_exc()
-            stack: str = _sanitize_traceback(raw_stack)
+            stack = traceback.format_exc()
             error = e
             exit_code = 1
         finally:
@@ -212,14 +209,3 @@ def _sanitize_argument_value(value: Enum | list[Any] | tuple[Any] | Any) -> Any:
         return [_sanitize_argument_value(item) for item in value]
 
     return value
-
-
-def _sanitize_traceback(raw_stack: str) -> str:
-    home = os.path.expanduser("~")
-    sanitized = raw_stack.replace(home, "<HOME>")
-    return sanitized
-
-
-def _create_install_id() -> str:
-    base_dir: str = os.path.expanduser("~")
-    platform_name = get_current_platform()[0]
