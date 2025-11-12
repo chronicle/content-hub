e1d9501ae1fc989d558ae2ce86ccb3cab57d42a0
haggit-eliyahu <haggit@google.com>
2025-11-12 13:35:39 +0200

    another example for testing using hypothesis

---
+++ packages/mp/tests/test_mp/test_core/test_data_models/test_action/test_dynamic_results_metadata.py
@@ -0,0 +1,98 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import annotations
+
+import json
+
+import pytest
+from hypothesis import given
+from hypothesis import strategies as st
+from pydantic import ValidationError
+
+from mp.core.data_models.action.dynamic_results_metadata import (
+    BuiltDynamicResultsMetadata,
+    DynamicResultsMetadata,
+    NonBuiltDynamicResultsMetadata,
+)
+
+
+def _is_not_valid_json(s: str) -> bool:
+    try:
+        if not s:
+            return True
+        json.loads(s)
+    except (json.JSONDecodeError, TypeError):
+        return True
+    else:
+        return False
+
+
+st_json_serializable = st.recursive(
+    st.none() | st.booleans() | st.floats(allow_nan=False) | st.text(),
+    lambda children: st.lists(children) | st.dictionaries(st.text(), children),
+)
+
+st_non_json_string = st.text().filter(_is_not_valid_json)
+
+st_valid_built_metadata_dict = st.fixed_dictionaries({
+    "ResultExample": st.none() | st_json_serializable.map(json.dumps) | st.just(""),
+    "ResultName": st.text(),
+    "ShowResult": st.booleans(),
+})
+
+st_valid_non_built_metadata_dict = st.fixed_dictionaries({
+    "result_example_path": st.none() | st_json_serializable.map(json.dumps),
+    "result_name": st.text(),
+    "show_result": st.booleans(),
+})
+
+
+st_invalid_built_dict = st.fixed_dictionaries({
+    "ResultExample": st_non_json_string.filter(lambda s: s != ""),
+    "ResultName": st.text(),
+    "ShowResult": st.booleans(),
+})
+
+st_invalid_non_built_dict = st.fixed_dictionaries({
+    "result_example_path": st_non_json_string.filter(lambda s: s != ""),
+    "result_name": st.text(),
+    "show_result": st.booleans(),
+})
+
+
+class TestValidations:
+    """
+    Tests for pydantic-level model validations.
+    """
+
+    @given(valid_non_built=st_valid_non_built_metadata_dict)
+    def test_valid_non_built(self, valid_non_built: NonBuiltDynamicResultsMetadata) -> None:
+        DynamicResultsMetadata.from_non_built(valid_non_built)
+
+    @given(valid_built=st_valid_built_metadata_dict)
+    def test_valid_built(self, valid_built: BuiltDynamicResultsMetadata) -> None:
+        DynamicResultsMetadata.from_built(valid_built)
+
+    @given(invalid_non_built=st_invalid_non_built_dict)
+    def test_invalid_non_built_fails(
+        self, invalid_non_built: NonBuiltDynamicResultsMetadata
+    ) -> None:
+        with pytest.raises((ValidationError, KeyError, ValueError)):
+            DynamicResultsMetadata.from_non_built(invalid_non_built).to_non_built()
+
+    @given(invalid_built=st_invalid_built_dict)
+    def test_invalid_built_fails(self, invalid_built: BuiltDynamicResultsMetadata) -> None:
+        with pytest.raises((ValidationError, ValueError, KeyError)):
+            DynamicResultsMetadata.from_built(invalid_built)
