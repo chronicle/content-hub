9555b78787c31fabe64a5699b260e7743d0fc8c6
haggit-eliyahu <haggit@google.com>
2025-11-17 16:21:42 +0200

    improve invalid structure

--- packages/mp/tests/test_mp/test_core/test_data_models/test_action/test_action_parameter.py
+++ packages/mp/tests/test_mp/test_core/test_data_models/test_action/test_action_parameter.py
@@ -26,7 +26,15 @@ from mp.core.data_models.action.parameter import (
     BuiltActionParameter,
     NonBuiltActionParameter,
 )
-from test_mp.test_core.test_data_models.utils import st_excluded_param_name, st_valid_param_name
+from test_mp.test_core.test_data_models.utils import (
+    st_excluded_param_name,
+    st_invalid_long_description,
+    st_invalid_long_param_name,
+    st_invalid_regex,
+    st_invalid_too_many_words_param_name,
+    st_valid_param_name,
+    st_with_invalid_field,
+)
 
 st_valid_non_built_action_param_dict = st.fixed_dictionaries(
     # Required keys
@@ -59,94 +67,50 @@ st_valid_built_action_param_dict = st.fixed_dictionaries(
     },
 )
 
+st_invalid_non_built_action_param_dict = st.one_of(
+    st_with_invalid_field(
+        st_valid_non_built_action_param_dict,
+        "name",
+        st_invalid_long_param_name(),
+    ),
+    st_with_invalid_field(st_valid_non_built_action_param_dict, "name", st_invalid_regex()),
+    st_with_invalid_field(
+        st_valid_non_built_action_param_dict,
+        "name",
+        st_invalid_too_many_words_param_name(),
+    ),
+    st_with_invalid_field(
+        st_valid_non_built_action_param_dict,
+        "description",
+        st_invalid_long_description(),
+    ),
+    st_with_invalid_field(st_valid_non_built_action_param_dict, "type", st.just("NOT_A_REAL_TYPE")),
+)
 
-@st.composite
-def _st_invalid_non_built_action_param_dict(draw: st.DrawFn) -> NonBuiltActionParameter:
-    # Start with a valid dict
-    data = draw(st_valid_non_built_action_param_dict)
-
-    # Pick a random way to break it
-    mutation = draw(
-        st.sampled_from([
-            "name_too_long",
-            "name_bad_pattern",
-            "name_too_many_words",
-            "desc_too_long",
-            "type_bad_string",
-        ])
-    )
-
-    # Apply the mutation
-    if mutation == "name_too_long":
-        data["name"] = draw(
-            st.text(min_size=mp.core.constants.PARAM_NAME_MAX_LENGTH + 1).filter(
-                lambda s: s not in mp.core.constants.EXCLUDED_PARAM_NAMES_WITH_TOO_MANY_WORDS
-            )
-        )
-    elif mutation == "name_bad_pattern":
-        data["name"] = draw(st.just("!@#$%^"))
-    elif mutation == "name_too_many_words":
-        data["name"] = draw(
-            st.text().filter(
-                lambda s: (
-                    len(s.split()) > mp.core.constants.PARAM_NAME_MAX_WORDS
-                    and s not in mp.core.constants.EXCLUDED_PARAM_NAMES_WITH_TOO_MANY_WORDS
-                )
-            )
-        )  # Fails AfterValidator
-    elif mutation == "desc_too_long":
-        data["description"] = draw(
-            st.text(min_size=mp.core.constants.SHORT_DESCRIPTION_MAX_LENGTH + 1)
-        )
-    elif mutation == "type_bad_string":
-        data["type"] = draw(st.just("NOT_A_REAL_TYPE"))
-
-    return data
-
-
-@st.composite
-def _st_invalid_built_action_param_dict(draw: st.DrawFn) -> BuiltActionParameter:
-    # Start with a valid dict
-    data = draw(st_valid_built_action_param_dict)
-
-    # Pick a random way to break it
-    mutation = draw(
-        st.sampled_from([
-            "name_too_long",
-            "name_bad_pattern",
-            "name_too_many_words",
-            "desc_too_long",
-            "type_bad_int",
-            "is_mandatory_bad_type",
-        ])
-    )
-
-    # Apply the mutation
-    if mutation == "name_too_long":
-        data["Name"] = draw(st.text(min_size=mp.core.constants.PARAM_NAME_MAX_LENGTH + 1))
-    elif mutation == "name_bad_pattern":
-        data["Name"] = draw(st.just("!@#$%^"))
-    elif mutation == "name_too_many_words":
-        data["Name"] = draw(
-            st.text().filter(
-                lambda s: (
-                    len(s.split()) > mp.core.constants.PARAM_NAME_MAX_WORDS
-                    and s not in mp.core.constants.EXCLUDED_PARAM_NAMES_WITH_TOO_MANY_WORDS
-                )
-            )
-        )
-    elif mutation == "desc_too_long":
-        data["Description"] = draw(
-            st.text(min_size=mp.core.constants.SHORT_DESCRIPTION_MAX_LENGTH + 1)
-        )
-    elif mutation == "type_bad_int":
-        data["Type"] = draw(
-            st.integers().filter(lambda n: n not in {t.value for t in ActionParamType}).map(str)
-        )
-    elif mutation == "is_mandatory_bad_type":
-        data["IsMandatory"] = draw(st.just("not a bool"))
-
-    return data
+st_invalid_built_action_param_dict = st.one_of(
+    st_with_invalid_field(
+        st_valid_built_action_param_dict,
+        "Name",
+        st_invalid_long_param_name(),
+    ),
+    st_with_invalid_field(st_valid_built_action_param_dict, "Name", st_invalid_regex()),
+    st_with_invalid_field(
+        st_valid_built_action_param_dict,
+        "Name",
+        st_invalid_too_many_words_param_name(),
+    ),
+    st_with_invalid_field(
+        st_valid_built_action_param_dict,
+        "Description",
+        st_invalid_long_description(),
+    ),
+    st_with_invalid_field(
+        st_valid_built_action_param_dict,
+        "Type",
+        st.integers().filter(lambda n: n not in {t.value for t in ActionParamType}).map(str),
+    ),
+    st_with_invalid_field(st_valid_built_action_param_dict, "IsMandatory", st.just("not a bool")),
+)
 
 
 class TestValidations:
@@ -187,13 +151,13 @@ class TestValidations:
         assert obj1 == obj2
 
     @settings(max_examples=30)
-    @given(invalid_non_built=_st_invalid_non_built_action_param_dict())
+    @given(invalid_non_built=st_invalid_non_built_action_param_dict)
     def test_invalid_non_built_fails(self, invalid_non_built: NonBuiltActionParameter) -> None:
         with pytest.raises((ValidationError, KeyError, ValueError)):
             ActionParameter.from_non_built(invalid_non_built)
 
     @settings(max_examples=30)
-    @given(invalid_built=_st_invalid_built_action_param_dict())
+    @given(invalid_built=st_invalid_built_action_param_dict)
     def test_invalid_built_fails(self, invalid_built: BuiltActionParameter) -> None:
         with pytest.raises((ValidationError, ValueError, KeyError)):
             ActionParameter.from_built(invalid_built)
