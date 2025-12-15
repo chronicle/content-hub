0dff1fb97441262f4dea0a398101421a47e3ef80
Amit Joseph <143286440+AmitJ98@users.noreply.github.com>
2025-12-15 12:22:58 +0200

    [Validation] Playbook - steps params validation (#368)
    * new validation, validate all parameters in steps for playbook
    
    * fixed msg
    
    * fixed gemini comments
    
    * fixed gemini comments #2
    
    * added tests for the validation
    
    * fixed tal coments

---
+++ packages/mp/tests/test_mp/test_validate/test_pre_build_validations/test_playbooks/common.py
@@ -0,0 +1,54 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import mp.core.constants
+import mp.core.file_utils
+from mp.core.data_models.playbooks.step.metadata import Step
+from mp.core.data_models.playbooks.step.step_parameter import StepParameter
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+
+def update_step_yaml(playbook_path: Path, updates: dict) -> None:
+    """Update step params and values."""
+    step: Step = Step.from_non_built_path(playbook_path)[0]
+
+    new_params = updates.pop("parameters")
+    if updates.get("is_automatic"):
+        step.is_automatic = updates["is_automatic"]
+    for new_param in new_params:
+        found = False
+        for existing_param in step.parameters:
+            if existing_param.name == new_param["name"]:
+                existing_param.value = new_param["value"]
+                found = True
+                break
+        if not found:
+            step.parameters.append(
+                StepParameter(
+                    name=new_param["name"],
+                    value=new_param["value"],
+                    step_id="1",
+                    playbook_id="1",
+                )
+            )
+    mp.core.file_utils.save_yaml(
+        step.to_non_built(),
+        playbook_path / mp.core.constants.STEPS_DIR / f"{step.instance_name}.yaml",
+    )
