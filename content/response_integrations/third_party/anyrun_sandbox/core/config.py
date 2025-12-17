91115b72f8f52738cb0d5c78e4c860fffc783c46
Semen Shalnev <85073445+semayellow@users.noreply.github.com>
2025-12-17 11:54:13 +0300

    fix(configs): bump integration version

--- content/response_integrations/third_party/anyrun_sandbox/core/config.py
+++ content/response_integrations/third_party/anyrun_sandbox/core/config.py
@@ -4,6 +4,6 @@ from __future__ import annotations
 class Config:
     """The main configuration class"""
 
-    VERSION: str = "Google-SecOps:1.1"
+    VERSION: str = "Google-SecOps:2.0"
     INTEGRATION_NAME: str = "ANYRUN-Sandbox"
     DATE_TIME_FORMAT: str = "%Y-%m-%d %H:%M:%S"
