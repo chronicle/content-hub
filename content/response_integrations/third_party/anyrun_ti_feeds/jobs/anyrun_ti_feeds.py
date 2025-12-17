cc50edc75d287cf1c948f475289b6cece3d83ddb
Semen Shalnev <85073445+semayellow@users.noreply.github.com>
2025-12-17 12:47:55 +0300

    [ANY.RUN]: Bug fixes, minor improvements (#376)
    * feat(anyrun): fix bugs, minor improvements
    
    * fix(configs): fix ai issues
    
    * fix(configs): bump integration version
    
    ---------
    
    Co-authored-by: Amit Joseph <143286440+AmitJ98@users.noreply.github.com>

--- content/response_integrations/third_party/anyrun_ti_feeds/jobs/anyrun_ti_feeds.py
+++ content/response_integrations/third_party/anyrun_ti_feeds/jobs/anyrun_ti_feeds.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
-from shlex import quote
-
 from soar_sdk.SiemplifyJob import SiemplifyJob
 from soar_sdk.SiemplifyUtils import output_handler
 from TIPCommon.extraction import extract_configuration_param, extract_job_param
@@ -16,8 +14,8 @@ def main():
 
     feed_fetch_depth = extract_job_param(siemplify, param_name="Feed Fetch Depth", input_type=int)
 
-    verify_ssl = quote(
-        extract_configuration_param(siemplify, Config.INTEGRATION_NAME, param_name="Verify SSL")
+    verify_ssl = extract_configuration_param(
+        siemplify, Config.INTEGRATION_NAME, param_name="Verify SSL", input_type=bool
     )
 
     data_tables = DataTableManager(siemplify)
