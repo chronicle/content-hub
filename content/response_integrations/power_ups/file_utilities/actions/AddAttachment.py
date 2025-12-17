21278e78e30fbfc4855ef676cb6fbd6b5aaa0c5d
kantkrishna <kantkrishna@google.com>
2025-12-17 13:09:31 +0000

    Fix Add Attachment/Get Attachement Json result.

--- content/response_integrations/power_ups/file_utilities/actions/AddAttachment.py
+++ content/response_integrations/power_ups/file_utilities/actions/AddAttachment.py
@@ -14,6 +14,7 @@
 
 from __future__ import annotations
 
+import dataclasses
 import json
 
 import requests
@@ -22,8 +23,51 @@ from soar_sdk.SiemplifyAction import SiemplifyAction
 from soar_sdk.SiemplifyUtils import output_handler
 from TIPCommon.data_models import CaseWallAttachment
 from TIPCommon.rest.soar_api import save_attachment_to_case_wall
+from TIPCommon.types import SingleJson
+
 
 SCRIPT_NAME = "Add Attachment"
+
+
+@dataclasses.dataclass(slots=True)
+class AttachmentResult:
+    raw_data: SingleJson
+
+    def to_json(self) -> SingleJson:
+        attachment_data = self.raw_data.pop("caseAttachment", {})
+        evidence_name = attachment_data.pop("fileName", self.raw_data.pop("evidenceName", None))
+        description = self.raw_data.pop("comment", self.raw_data.pop("description", None))
+        evidence_thumbnail_base64 = self.raw_data.pop("evidenceThumbnailBase64", None)
+        evidence_id = attachment_data.pop("attachmentId", self.raw_data.pop("evidenceId", 0))
+        file_type = attachment_data.pop("fileType", self.raw_data.pop("fileType", None))
+        creator_user_id = self.raw_data.pop("user", self.raw_data.pop("creatorUserId", None))
+        id_ = self.raw_data.pop("id", None)
+        case_id = self.raw_data.pop("case", self.raw_data.pop("caseId", -1))
+        is_favorite = self.raw_data.pop("isFavorite", False)
+        update_time = self.raw_data.pop(
+            "updateTime", self.raw_data.pop("modificationTimeUnixTimeInMs", -1)
+        )
+        create_time = self.raw_data.pop(
+            "createTime", self.raw_data.pop("creationTimeUnixTimeInMs", -1)
+        )
+        alert_identifier = self.raw_data.pop("alertIdentifier", None)
+        return {
+            "evidenceName": evidence_name,
+            "description": description,
+            "evidenceThumbnailBase64": evidence_thumbnail_base64,
+            "evidenceId": evidence_id,
+            "fileType": file_type,
+            "creatorUserId": creator_user_id,
+            "id": id_,
+            "type": 4,
+            "caseId": case_id,
+            "isFavorite": is_favorite,
+            "modificationTimeUnixTimeInMs": update_time,
+            "creationTimeUnixTimeInMs": create_time,
+            "alertIdentifier": alert_identifier,
+            **self.raw_data,
+            **attachment_data,
+        }
 
 
 @output_handler
@@ -45,9 +89,8 @@ def main():
         description=description,
     )
     try:
-        attachment = save_attachment_to_case_wall(
-            chronicle_soar=siemplify,
-            attachment_data=attachment_data
+        attachment = AttachmentResult(
+            save_attachment_to_case_wall(chronicle_soar=siemplify, attachment_data=attachment_data)
         )
 
     except requests.HTTPError as e:
@@ -56,7 +99,7 @@ def main():
         output_message = f'Error executing action "{SCRIPT_NAME}": {e}'
         siemplify.end(output_message, False, EXECUTION_STATE_FAILED)
 
-    json_response = attachment
+    json_response = attachment.to_json()
 
     siemplify.result.add_result_json(json.dumps(json_response))
 
