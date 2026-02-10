5db9f79f8db4e5bcb389672cbe8ec8c98e8047c9
Sumit Khobragade <sumit.khobragade@metronlabs.com>
2026-02-10 08:48:11 +0000

    fix: added the required variables

--- packages/mp/src/mp/core/constants.py
+++ packages/mp/src/mp/core/constants.py
@@ -82,9 +82,9 @@ OUT_CONNECTORS_META_DIR: str = "Connectors"
 OUT_JOBS_META_DIR: str = "Jobs"
 OUT_WIDGETS_META_DIR: str = "Widgets"
 
-AI_DIR: str = "ai"
+AI_FOLDER: str = "ai"
 ACTIONS_AI_DESCRIPTION_FILE: str = "actions_ai_description.yaml"
-
+AI_META_JSON_FILE: str = "actions_ai_metadata.json"
 ACTIONS_META_SUFFIX: str = ".actiondef"
 CONNECTORS_META_SUFFIX: str = ".connectordef"
 JOBS_META_SUFFIX: str = ".jobdef"
@@ -236,4 +236,4 @@ PLAYBOOK_MUST_HAVE_KEYS: set[str] = {
     "OverviewTemplatesDetails",
     "WidgetTemplates",
     "Definition",
-}    
+}
