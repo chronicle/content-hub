6bd8fcaa4a07486aaaefdbb9f007d6b049d5ca7e
talshapir <talshapir@google.com>
2026-02-12 10:33:07 +0200

    commit some stuff (bad name.. I know)

--- packages/deploy_agent/main.py
+++ packages/deploy_agent/main.py
@@ -1,3 +1,17 @@
+# Copyright 2026 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import argparse
 import logging
 import os
@@ -10,6 +24,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 from vertexai.agent_engines import AdkApp
 
 from .log import setup_logging
+from .settings import Settings, get_settings
 
 logger: logging.Logger = logging.getLogger("product_agent_mesh.deployment")
 
--- packages/deploy_agent/mcps/servicenow.sh
+++ packages/deploy_agent/mcps/servicenow.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+
+# Copyright 2026 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
@@ -16,4 +31,4 @@ export PATH="$HOME/.local/bin:$PATH"
 # Install the mcp-reddit tool using the command from its documentation
 echo "Installing mcp-reddit using uv..."
 uv pip install "git+https://github.com/michaelbuckner/servicenow-mcp.git" --system
-echo "MCP Reddit Server installation complete."
+echo "MCP ServiceNow Server installation complete."
