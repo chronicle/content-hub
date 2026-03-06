dbfcf7e455cc7953c82d1d2e0fa79de3596d2107
gnishant <gnishant@google.com>
2026-03-06 06:20:14 +0000

    tipcommon file update

--- packages/tipcommon/TIPCommon/src/TIPCommon/rest/soar_platform_clients/one_platform_soar_api.py
+++ packages/tipcommon/TIPCommon/src/TIPCommon/rest/soar_platform_clients/one_platform_soar_api.py
@@ -819,7 +819,8 @@ class OnePlatformSoarApi(BaseSoarApi):
     def export_package(self) -> requests.Response:
         """Export package"""
         endpoint = (
-            f"/integrations/{self.params.integration_identifier}:export?alt=media"
+            f"/download/integrations/{self.params.integration_identifier}:"
+            "export?alt=media"
         )
         return self._make_request(HttpMethod.GET, endpoint).content
 
