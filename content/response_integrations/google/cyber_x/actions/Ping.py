3ac7a794671d0fe1db80afda7a21c6697128983e
haggit-eliyahu <haggit@google.com>
2026-03-10 10:25:46 +0200

    migrate cyber_x

---
+++ content/response_integrations/google/cyber_x/actions/Ping.py
@@ -0,0 +1,26 @@
+
+ACTION_NAME = "CyberX_Ping"
+PROVIDER = "CyberX"
+
+
+@output_handler
+def main():
+    siemplify = SiemplifyAction()
+    siemplify.script_name = ACTION_NAME
+
+    config = siemplify.get_configuration(PROVIDER)
+    api_root = config["API Root"]
+    access_token = config["Access Token"]
+    verify_ssl = config.get("Verify SSL", "false").lower() == "true"
+
+    cyberx_manager = CyberXManager(
+        api_root=api_root, access_token=access_token, verify_ssl=verify_ssl
+    )
+
+    cyberx_manager.get_all_devices()
+
+    siemplify.end("Connection established.", True)
+
+
+if __name__ == "__main__":
+    main()
