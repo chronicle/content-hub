86b82cd01076f0030fb26e1a307131d0b7be05d9
talshapir <talshapir@google.com>
2026-03-19 16:23:31 +0200

    Change the new field's name

--- packages/mp/src/mp/core/data_models/integrations/integration_meta/metadata.py
+++ packages/mp/src/mp/core/data_models/integrations/integration_meta/metadata.py
@@ -108,7 +108,7 @@ class BuiltIntegrationMetadata(TypedDict):
     IsCustom: bool
     IsPowerUp: bool
     IsCertified: bool
-    IsFirstParty: NotRequired[bool]
+    GoogleSecOpsProduct: NotRequired[bool]
 
 
 class NonBuiltIntegrationMetadata(TypedDict):
@@ -127,7 +127,7 @@ class NonBuiltIntegrationMetadata(TypedDict):
     is_custom: NotRequired[bool]
     is_available_for_community: NotRequired[bool]
     is_powerup: NotRequired[bool]
-    is_first_party: NotRequired[bool]
+    google_secops_product: NotRequired[bool]
 
 
 class IntegrationMetadata(
@@ -173,7 +173,7 @@ class IntegrationMetadata(
         pydantic.PositiveFloat,
         pydantic.Field(ge=MINIMUM_SYSTEM_VERSION),
     ] = MINIMUM_SYSTEM_VERSION
-    is_first_party: bool = False
+    google_secops_product: bool = False
 
     @classmethod
     def from_built_path(cls, path: Path) -> Self:
@@ -260,7 +260,7 @@ class IntegrationMetadata(
             is_custom=built.get("IsCustom", False),
             is_available_for_community=built.get("IsAvailableForCommunity", True),
             is_powerup=built.get("IsPowerUp", False),
-            is_first_party=built.get("IsFirstParty", False),
+            google_secops_product=built.get("GoogleSecOpsProduct", False),
         )
 
     @classmethod
@@ -290,7 +290,7 @@ class IntegrationMetadata(
                 True,
             ),
             is_powerup=non_built.get("is_powerup", False),
-            is_first_party=non_built.get("is_first_party", False),
+            google_secops_product=non_built.get("google_secops_product", False),
         )
 
     def to_built(self) -> BuiltIntegrationMetadata:
@@ -327,7 +327,7 @@ class IntegrationMetadata(
             IsCustom=self.is_custom,
             IsPowerUp=self.is_powerup,
             IsCertified=self.is_certified,
-            IsFirstParty=self.is_first_party,
+            GoogleSecOpsProduct=self.google_secops_product,
         )
         mp.core.utils.remove_none_entries_from_mapping(built)
         return built
@@ -370,8 +370,8 @@ class IntegrationMetadata(
         if self.is_powerup:
             non_built["is_powerup"] = self.is_powerup
 
-        if self.is_first_party:
-            non_built["is_first_party"] = self.is_first_party
+        if self.google_secops_product:
+            non_built["google_secops_product"] = self.google_secops_product
 
         mp.core.utils.remove_none_entries_from_mapping(non_built)
         return non_built
