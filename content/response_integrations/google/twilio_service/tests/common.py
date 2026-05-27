a239dc7c2d4baac7f352d59c5aa843b1b910098a
haggit-eliyahu <haggit@google.com>
2026-05-27 08:16:38 +0300

    fix tests problems

--- content/response_integrations/google/twilio_service/tests/common.py
+++ content/response_integrations/google/twilio_service/tests/common.py
@@ -12,3 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from __future__ import annotations
+import pathlib
+import json
+
+INTEGRATION_PATH: pathlib.Path = pathlib.Path(__file__).parent.parent
+CONFIG_PATH: pathlib.Path = pathlib.Path(__file__).parent / 'config.json'
+CONFIG: dict = json.loads(CONFIG_PATH.read_text()) if CONFIG_PATH.exists() else {}
