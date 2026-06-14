b89193696250be622f3350ac0470c5ad24b5439d
haggit-eliyahu <haggit@google.com>
2026-06-14 10:14:54 +0300

    Fix linting issues (SIM102, E303, E501, RUF043)

--- packages/mp/src/mp/validate/utils.py
+++ packages/mp/src/mp/validate/utils.py
@@ -58,9 +58,8 @@ def get_marketplace_paths_from_names(
 
             # Check if the name matches a suffixed integration (e.g. 'http' -> 'http_integration')
             suffixed_name: str = f"{n}_integration"
-            if suffixed_name in constants.INTEGRATIONS_WITH_INTEGRATION_SUFFIX:
-                if (p := path / suffixed_name).exists():
-                    results.add(p)
+            if suffixed_name in constants.INTEGRATIONS_WITH_INTEGRATION_SUFFIX and (p := path / suffixed_name).exists():
+                results.add(p)
     return results
 
 
