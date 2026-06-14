59ec7d8bf905e223cb479b7734d331280b47163c
Amit Joseph <amitjoseph@google.com>
2026-06-14 15:19:50 +0300

    new google integrations action ai meta

--- packages/mp/tests/test_mp/test_core/test_data_models/test_action/test_action_metadata.py
+++ packages/mp/tests/test_mp/test_core/test_data_models/test_action/test_action_metadata.py
@@ -27,7 +27,7 @@ from mp.core.data_models.integrations.action.metadata import (
     ActionMetadata,
     BuiltActionMetadata,
     NonBuiltActionMetadata,
-    _determine_ai_categories,
+    _determine_ai_categories,  # noqa: PLC2701
     _load_json_examples,  # noqa: PLC2701
 )
 from test_mp.test_core.test_data_models.utils import FILE_NAME
