4d0eb848d7857ff098a05ecce59628360e1b7b69
haggit-eliyahu <haggit@google.com>
2026-06-29 10:00:12 +0300

    delete sec ops ai agents integration

--- content/response_integrations/google/google_sec_ops_ai_agents/test_patch_utils.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from soar_sdk.SiemplifyAction import SiemplifyAction
-from TIPCommon.base.utils import create_soar_action
-
-def test_triage():
-    action = create_soar_action()
-    print("Action type:", type(action))
