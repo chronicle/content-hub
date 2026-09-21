75c44345316468dd1e2c7696e4f4e7b760a52314
Arabindaksha Mishra <109797459+Arabindaksha-Mishra@users.noreply.github.com>
2026-09-21 05:01:21 +0000

    [b/558240313] SentinelOne Singularity Operations Center: Scope DestinationHostName ontology mapping to Asset events

--- content/response_integrations/google/sentinel_one_singularity_operations_center/tests/common.py
+++ content/response_integrations/google/sentinel_one_singularity_operations_center/tests/common.py
@@ -26,3 +26,5 @@ if TYPE_CHECKING:
 INTEGRATION_PATH: pathlib.Path = pathlib.Path(__file__).parent.parent
 CONFIG_PATH = pathlib.Path.joinpath(INTEGRATION_PATH, "tests", "config.json")
 CONFIG: SingleJson = get_def_file_content(CONFIG_PATH)
+
+ONTOLOGY_PATH = pathlib.Path.joinpath(INTEGRATION_PATH, "ontology_mapping.yaml")
